f0df75ca34365843259ebff88bb1e8f76faba81d
Darman <darmanyamanh10@gmail.com>
2022-04-28 20:20:34 +0700

    commit -m "authentication implemented"

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,93 +1,95 @@
-const { nanoid } = require("nanoid");
-const { Pool } = require("pg");
-const InvariantError = require("../../exceptions/InvariantError");
+/* eslint-disable max-len */
+/* eslint-disable no-underscore-dangle */
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
-    constructor() {
-        this._pool = new Pool();
-    }
+  constructor() {
+    this._pool = new Pool();
+  }
 
-    // Ingat! fungsi query() berjalan secara asynchronous,
-    // dengan begitu kita perlu menambahkan async pada addNote dan await pada pemanggilan query()
-    async addNote({ title, body, tags }) {
-        const id = nanoid(16);
-        const createdAt = new Date().toISOString();
-        const updatedAt = createdAt;
-        
-        // buat objek query untuk memasukan notes baru ke database
-        const query = {
-          text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-          values: [id, title, body, tags, createdAt, updatedAt],
-        };
+  // Ingat! fungsi query() berjalan secara asynchronous,
+  // dengan begitu kita perlu menambahkan async pada addNote dan await pada pemanggilan query()
+  async addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
 
-        // mengeksekusi query yang sudah dibuat
-        const result = await this._pool.query(query);
-    
-        // evaluasi nilai dari results.rows[0].id (karena kita melakukan returning id pada query)
-        /** Jika nilai id tidak undefined, itu berarti catatan berhasil dimasukan dan kembalikan
+    // buat objek query untuk memasukan notes baru ke database
+    const query = {
+      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, title, body, tags, createdAt, updatedAt],
+    };
+
+    // mengeksekusi query yang sudah dibuat
+    const result = await this._pool.query(query);
+
+    // evaluasi nilai dari results.rows[0].id (karena kita melakukan returning id pada query)
+    /** Jika nilai id tidak undefined, itu berarti catatan berhasil dimasukan dan kembalikan
          * fungsi dengan nilai id. Jika tidak maka throw InvariantError. */
-        if (!result.rows[0].id) {
-            throw new InvariantError('Catatan gagal ditambahkan');
-        }
-    
-        return result.rows[0].id;
+    if (!result.rows[0].id) {
+      throw new InvariantError('Catatan gagal ditambahkan');
     }
 
-    async getNotes() {
-        const result = await this._pool.query('SELECT * FROM notes');
-        return result.rows.map(mapDBToModel);
+    return result.rows[0].id;
+  }
+
+  async getNotes() {
+    const result = await this._pool.query('SELECT * FROM notes');
+    return result.rows.map(mapDBToModel);
+  }
+
+  async getNoteById(id) {
+    // mendapatkan note di dalam database berdasarkan id yang diberikan
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    // periksa nilai result.rows, bila nilainya 0 (false) maka bangkitkan NotFoundError.
+    // Bila tidak, maka kembalikan dengan result.rows[0] yang sudah di-mapping dengan fungsi mapDBToModel
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan');
     }
 
-    async getNoteById(id) {
-        // mendapatkan note di dalam database berdasarkan id yang diberikan
-        const query = {
-            text: 'SELECT * FROM notes WHERE id = $1',
-            values: [id],
-        };
+    return result.rows.map(mapDBToModel)[0];
+  }
 
-        const result = await this._pool.query(query);
+  async editNoteById(id, { title, body, tags }) {
+    const updatedAt = new Date().toISOString();
+    // query untuk mengubah note di dalam database berdasarkan id yang diberikan
+    const query = {
+      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+      values: [title, body, tags, updatedAt, id],
+    };
 
-        /**periksa nilai result.rows, bila nilainya 0 (false) maka bangkitkan NotFoundError.
-         * Bila tidak, maka kembalikan dengan result.rows[0] yang sudah di-mapping dengan fungsi mapDBToModel */
-        if (!result.rows.length) {
-            throw new NotFoundError('Catatan tidak ditemukan');
-        }
+    const result = await this._pool.query(query);
 
-        return result.rows.map(mapDBToModel)[0];
+    // periksa nilai result.rows bila nilainya 0 (false) maka bangkitkan NotFoundError
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbaharui catatan. Id tidak ditemukan');
     }
+  }
 
-    async editNoteById(id, {title, body, tags}) {
-        const updatedAt = new Date().toISOString();
-        // query untuk mengubah note di dalam database berdasarkan id yang diberikan
-        const query = {
-        text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
-        values: [title, body, tags, updatedAt, id],
-        };
-    
-        const result = await this._pool.query(query);
+  async deleteNoteById(id) {
+    // query untuk menghapus note di dalam database berdasarkan id yang diberikan
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
 
-        // periksa nilai result.rows bila nilainya 0 (false) maka bangkitkan NotFoundError
-        if (!result.rows.length) {
-            throw new NotFoundError('Gagal memperbaharui catatan. Id tidak ditemukan');
-        }
+    const result = await this._pool.query(query);
+
+    // periksa nilai result.rows bila nilainya 0 (false) maka bangkitkan NotFoundError
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
     }
-
-    async deleteNoteById(id) {
-        // query untuk menghapus note di dalam database berdasarkan id yang diberikan
-        const query = {
-            text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
-            values: [id],
-        };
-
-        const result = await this._pool.query(query);
-
-        // periksa nilai result.rows bila nilainya 0 (false) maka bangkitkan NotFoundError
-        if (!result.rows.length) {
-            throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
-        }
-    }
+  }
 }
 
 module.exports = NotesService;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,19 @@
+/* eslint-disable camelcase */
 //  fungsi bernama mapDBToModel dan menerima parameter note objek dari database.
 const mapDBToModel = ({
-    id,
-    title,
-    body,
-    tags,
-    created_at,
-    updated_at,
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
 }) => ({
-    id,
-    title,
-    body,
-    tags,
-    createdAt: created_at,
-    updatedAt: updated_at,
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 module.exports = { mapDBToModel };
